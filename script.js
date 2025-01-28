07b8d9052515c498775835ad23bffcbf7e9a563d
ricardo1slas <rislas837@gmail.com>
2025-01-28 22:50:27 +0100

    sadas

---
+++ script.js
@@ -0,0 +1,33 @@
+const images = [
+    "assets/77C2062D-21FF-4341-B7F6-95B4A5E624CD_1_105_c.jpeg",
+    "assets/A8305F9A-92D1-41D4-83B7-34D9CAF5B818_1_105_c.jpeg",
+    "assets/B439B10E-455A-4286-AA02-C75BF49BA819_1_105_c.jpeg",
+    "assets/2C5E9928-876B-4669-BC2E-904694E89F52_1_105_c.jpeg"
+];
+
+let index = 0;
+
+function changeBackground() {
+    document.querySelector(".hero").style.backgroundImage = `url(${images[index]})`;
+    index = (index + 1) % images.length; // Loop back to first image after last
+}
+
+setInterval(changeBackground, 5000); // Change image every 5 seconds
+
+let lastScrollTop = 0;
+const navbar = document.querySelector("nav");
+const navLinks = document.querySelector(".nav-links");
+
+window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling Down - Hide the menu links
+        navLinks.style.display = "none";
+    } else {
+        // Scrolling Up - Show the menu links
+        navLinks.style.display = "flex";
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
